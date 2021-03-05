e9f2ecdf9c0e9b61675ce5c58dbe31248433e075
Ironolife <ironolifeosrs@gmail.com>
2021-03-06 00:48:38 +0800

    Switched from Serverless to VPS 🚧

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import pk from 'package.json';
-import serverless from 'serverless-http';
 import ytdl from 'ytdl-core';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -9,62 +8,69 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-  })
-);
+const main = async () => {
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+    })
+  );
 
-app.get('/version', async (_, res) => {
-  res.status(200).send(pk.version);
-});
+  app.get('/version', async (_, res) => {
+    res.status(200).send(pk.version);
+  });
 
-app.get('/formats', async (req, res) => {
-  try {
-    const url = req.query.url;
-    if (typeof url !== 'string' || !ytdl.validateURL(url)) {
-      throw new Error('Invalid Url');
+  app.get('/formats', async (req, res) => {
+    try {
+      const url = req.query.url;
+      if (typeof url !== 'string' || !ytdl.validateURL(url)) {
+        throw new Error('Invalid Url');
+      }
+
+      const info = await ytdl.getInfo(url);
+
+      res.status(200).send(info.formats);
+    } catch (err) {
+      console.error(err);
+
+      res.status(500).send(err.message);
     }
+  });
 
-    const info = await ytdl.getInfo(url);
+  app.get('/download', async (req, res) => {
+    try {
+      const url = req.query.url;
+      if (typeof url !== 'string') {
+        throw new Error('Invalid Url');
+      }
 
-    res.status(200).send(info.formats);
-  } catch (err) {
-    console.error(err);
+      const itag = parseInt(req.query.itag as string, 10);
+      if (isNaN(itag)) {
+        throw new Error('Invalid Format');
+      }
 
-    res.status(500).send(err.message);
-  }
-});
+      const info = await ytdl.getInfo(url);
+      const format = info.formats.find((format) => format.itag === itag);
 
-app.get('/download', async (req, res) => {
-  try {
-    const url = req.query.url;
-    if (typeof url !== 'string') {
-      throw new Error('Invalid Url');
+      if (!format) {
+        throw new Error('Format not found');
+      }
+
+      res.header(
+        'Content-Disposition',
+        `attachment; filename="${format.qualityLabel}.${format.container}"`
+      );
+      ytdl(url, { format }).pipe(res);
+    } catch (err) {
+      console.error(err);
+
+      res.status(500).send(err.message);
     }
+  });
 
-    const itag = parseInt(req.query.itag as string, 10);
-    if (isNaN(itag)) {
-      throw new Error('Invalid Format');
-    }
+  const port = parseInt(process.env.PORT, 10);
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}.`);
+  });
+};
 
-    const info = await ytdl.getInfo(url);
-    const format = info.formats.find((format) => format.itag === itag);
-
-    if (!format) {
-      throw new Error('Format not found');
-    }
-
-    res.header(
-      'Content-Disposition',
-      `attachment; filename="${format.qualityLabel}.${format.container}"`
-    );
-    ytdl(url, { format }).pipe(res);
-  } catch (err) {
-    console.error(err);
-
-    res.status(500).send(err.message);
-  }
-});
-
-module.exports.handler = serverless(app);
+main();
