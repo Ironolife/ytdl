17ba773bea10bb5d0c02ad7f9a32ab0010ddfa69
Ironolife <ironolifeosrs@gmail.com>
2021-03-08 23:43:37 +0800

    Improved ci workflow

--- packages/web/src/components/Footer/Footer.tsx
+++ packages/web/src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className='bg-violet-700 text-white text-center p-4 flex-shrink-0'>
+  <footer className='p-4 flex-shrink-0 text-white text-center bg-violet-700'>
     <a href='https://github.com/Ironolife' target='_black' rel='noreferrer'>
       Made with <span className='text-pink-500'>♥</span> by Ironolife © 2021
     </a>
