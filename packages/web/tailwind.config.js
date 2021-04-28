59784e25e52739b8918c90f72d33744a3c179184
Ironolife <40685147+Ironolife@users.noreply.github.com>
2021-04-28 18:25:42 +0800

    Updated tailwind config

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -2,10 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      sans: ['Roboto', 'sans-serif'],
-    },
     extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
       colors: {
         violet: {
           100: '#e8d5f9',
